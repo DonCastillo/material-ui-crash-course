f132e38851d76330b748be596cf14f6be592a989
Don Castillo <don.castillo@uleth.ca>
2021-03-11 17:46:47 -0700

    added button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Button
+          startIcon={<SaveIcon />}
+          size="large"
+					onClick={() => alert("Hello")}
+					variant="contained"
+					color="secondary"
+				>
+					Hello World
+				</Button>
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+		</div>
+	);
 }
 
 export default App;
