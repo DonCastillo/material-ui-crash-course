c319b254ecba4f9db912ad36e2fcb97a80b401cb
Don Castillo <don.castillo@uleth.ca>
2021-03-11 23:01:29 -0700

    added appbar

--- src/App.js
+++ src/App.js
@@ -8,19 +8,30 @@ import Checkbox from "@material-ui/core/Checkbox";
 import DeleteIcon from "@material-ui/icons/Delete";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from "@material-ui/core/TextField";
-import { makeStyles, ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import {green, orange} from "@material-ui/core/colors/";
+import {
+	makeStyles,
+	ThemeProvider,
+	createMuiTheme,
+} from "@material-ui/core/styles";
+import { green, orange } from "@material-ui/core/colors/";
+import "fontsource-roboto";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: green[500]
-    },
-    secondary: {
-      main: orange[500]
-    }
-  }
-})
+	typography: {
+		h2: {
+			fontSize: 18,
+			marginBottom: 0,
+		},
+	},
+});
 const useStyles = makeStyles({
 	root: {
 		background: "linear-gradient(45deg, #FE6B8B, #FF8E53)",
@@ -62,30 +73,62 @@ function CheckboxExample() {
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<div className="App">
-				<header className="App-header">
-					<ButtonStyled />
-					<TextField
-						variant="filled"
-						color="secondary"
-						type="email"
-						label="The Time"
-						placeholder="test@test.com"
-					/>
-					<CheckboxExample />
-					<ButtonGroup
-						variant="contained"
-						color="primary"
-						size="large"
-					>
-						<Button startIcon={<SaveIcon />}>Save</Button>
+			<Container maxWidth="xs">
+				<div className="App">
+					<header className="App-header">
+						<AppBar color="secondary">
+							<Toolbar>
+								<IconButton>
+									<MenuIcon />
+								</IconButton>
+								<Typography variant="h6">
+									MUI Theming
+								</Typography>
+								<Button>Login</Button>
+							</Toolbar>
+						</AppBar>
+						<Typography variant="h2" component="div">
+							Welcome to MUI
+						</Typography>
+						<Typography variant="subtitle1">
+							Learn how to use Material UI
+						</Typography>
+						<ButtonStyled />
 
-						<Button startIcon={<DeleteIcon />}>Discard</Button>
-					</ButtonGroup>
+						<Grid container spacing={2} justify="center">
+							<Grid item xs={3} sm={6}>
+								<Paper style={{ height: 75, width: "100%" }} />
+							</Grid>
+							<Grid item xs={3} sm={6}>
+								<Paper style={{ height: 75, width: "100%" }} />
+							</Grid>
+							<Grid item xs={3} sm={6}>
+								<Paper style={{ height: 75, width: "100%" }} />
+							</Grid>
+						</Grid>
 
-					<img src={logo} className="App-logo" alt="logo" />
-				</header>
-			</div>
+						<TextField
+							variant="filled"
+							color="secondary"
+							type="email"
+							label="The Time"
+							placeholder="test@test.com"
+						/>
+						<CheckboxExample />
+						<ButtonGroup
+							variant="contained"
+							color="primary"
+							size="large"
+						>
+							<Button startIcon={<SaveIcon />}>Save</Button>
+
+							<Button startIcon={<DeleteIcon />}>Discard</Button>
+						</ButtonGroup>
+
+						<img src={logo} className="App-logo" alt="logo" />
+					</header>
+				</div>
+			</Container>
 		</ThemeProvider>
 	);
 }
